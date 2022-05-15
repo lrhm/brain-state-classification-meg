52f37a72e06c30b7f0e3ed4ae0891bdb946d4030
GiulioZani <giulio.zani@gmail.com>
2022-05-15 23:33:29 +0200

    per subject metrics

--- dl/base_lightning_modules/base_classification_model.py
+++ dl/base_lightning_modules/base_classification_model.py
@@ -6,8 +6,40 @@ from argparse import Namespace
 import matplotlib.pyplot as plt
 import ipdb
 import os
-import torchmetrics
-from torchmetrics.classification import accuracy
+from torchmetrics import Accuracy
+
+
+class SubjectAccuracies:
+    def __init__(self, device: t.device):
+        self.device = device
+        self.accuracies = []
+
+    def label_to_one_hot(self, labels: t.Tensor):
+        labels = labels.to(self.device)
+        labels = t.eye(4)[labels].to(self.device)
+        return labels
+
+    def one_hot_to_label(self, one_hot: t.Tensor):
+        return t.argmax(one_hot)
+
+    def compute(self):
+        return [acc.compute() for acc in self.accuracies]
+
+    def reset(self):
+        for acc in self.accuracies:
+            acc.reset()
+
+    def update(
+        self, subject_label: t.Tensor, predicted_task: t.Tensor, task: t.Tensor,
+    ):
+        while ((subject_label + 1) > len(self.accuracies)).any():
+            self.accuracies.append(Accuracy())
+        for i, acc in enumerate(self.accuracies):
+            subject_mask = subject_label == i
+            predicted_task_label = predicted_task[subject_mask]
+            task_label = task[subject_mask]
+            if len(task_label) > 0:
+                acc.update(predicted_task_label.to(self.device), task_label.to(self.device))
 
 
 class BaseClassificationModel(LightningModule):
@@ -17,60 +49,93 @@ class BaseClassificationModel(LightningModule):
         self.save_hyperparameters()
         self.generator = t.nn.Sequential()
         self.loss = t.nn.BCELoss()
-        self.val_accuracy = torchmetrics.Accuracy()
-        self.test_accuracy = torchmetrics.Accuracy()
+        self.train_accuracies = SubjectAccuracies(self.device)
+        self.val_accuracies = SubjectAccuracies(self.device)
+        self.test_accuracies = SubjectAccuracies(self.device)
 
     def forward(self, z: t.Tensor) -> t.Tensor:
         out = self.generator(z)
         return out
 
+    def label_to_one_hot(self, labels: t.Tensor):
+        labels = labels.long()
+        labels = labels.to(self.device)
+        labels = t.eye(4)[labels].to(self.device)
+        return labels
+
+    def one_hot_to_label(self, one_hot: t.Tensor):
+        return t.argmax(one_hot, dim=1)
+
     def training_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
-        x, y = batch
+        x, labels = batch
+        task = labels[:, 0]
+        subject_label = labels[:, 1]
+        y = self.label_to_one_hot(task)
         y_pred = self(x)
         loss = self.loss(y_pred, y)
-        return {"loss": loss}
-
-    def validation_epoch_end(self, outputs):
-        acc = self.val_accuracy.compute()
-        self.log("accuracy", acc, prog_bar=True)
-        self.log("val_loss", 1 - acc, prog_bar=True)
-        self.val_accuracy.reset()
-        t.save(
-            self.state_dict(), os.path.join(self.params.save_path, "checkpoint.ckpt"),
+        self.train_accuracies.update(
+            subject_label=subject_label,
+            predicted_task=self.one_hot_to_label(y_pred).int(),
+            task=self.one_hot_to_label(y).int(),
         )
-        return {"val_loss": acc}
+        return {"loss": loss}
 
     def training_epoch_end(self, outputs):
         avg_loss = t.stack([x["loss"] for x in outputs]).mean()
+        self.log("avg_train_loss", avg_loss, prog_bar=True)
+        accuracies = self.train_accuracies.compute()
+        for i, acc in enumerate(accuracies):
+            self.log(
+                f"train_acc_{i}", acc, prog_bar=True,
+            )
+        self.train_accuracies.reset()
 
     def validation_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
-        x, y = batch
+        x, labels = batch
+        task = labels[:, 0].int()
+        patient_label = labels[:, 1]
         if batch_idx == 0:
             pass
         pred_y = self(x)
-        maxes = t.argmax(pred_y, dim=1)
-        pred_y = t.eye(4)[maxes].to(self.device)
-        y = y.int()
-        self.val_accuracy.update(pred_y, y)
+        y = self.label_to_one_hot(task)
+        pred_task = self.one_hot_to_label(pred_y)
+        self.val_accuracies.update(
+            subject_label=patient_label, predicted_task=pred_task, task=task,
+        )
+        return {"val_loss": self.loss(y, pred_y)}
+
+    def validation_epoch_end(self, outputs):
+        accuracies = self.val_accuracies.compute()
+        for i, acc in enumerate(accuracies):
+            self.log(
+                f"val_acc_{i}", acc, prog_bar=True,
+            )
+        self.val_accuracies.reset()
+        t.save(
+            self.state_dict(), os.path.join(self.params.save_path, "checkpoint.ckpt"),
+        )
+        avg_loss = t.stack([x["val_loss"] for x in outputs]).mean()
+        self.log("val_loss", avg_loss, prog_bar=True)
+        return {"val_loss": avg_loss}
 
     def test_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
-        x, y = batch
+        x, labels = batch
+        task = labels[:, 0].long()
+        patient_label = labels[:, 1]
         if batch_idx == 0:
             pass
-        pred_y = self(x)
-        maxes = t.argmax(pred_y, dim=1)
-        pred_y = t.eye(4)[maxes].to(self.device)
-        y = y.int()
-        self.test_accuracy.update(pred_y, y)
-        return
+        pred_y = self.one_hot_to_label(self(x))
+        self.test_accuracies.update(
+            subject_label=patient_label, predicted_task=pred_y, task=task,
+        )
 
     def test_epoch_end(self, outputs):
-        accuracy = self.test_accuracy.compute()
-        self.test_accuracy.reset()
-        test_metrics = {
-            "accuracy": accuracy,
-        }
-        test_metrics = {k: v for k, v in test_metrics.items()}
+        accuracies = self.val_accuracies.compute()
+        self.val_accuracies.reset()
+        t.save(
+            self.state_dict(), os.path.join(self.params.save_path, "checkpoint.ckpt"),
+        )
+        test_metrics = {f"{i}": acc for i, acc in enumerate(accuracies)}
         self.log("test_performance", test_metrics, prog_bar=True)
 
     def configure_optimizers(self):
